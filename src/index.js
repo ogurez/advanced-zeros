5112df06413675812742f29b74da1346b15ce53f
Ray_Bruges <ogurezslava@gmail.com>
2018-09-26 23:01:01 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  function getSimpleMn(num){
+    let arr = [];
+    if(num > 0 && num != 1){
+      for(let i = 2; i <= num; i++){
+        while(num % i == 0 && num > 1){
+          arr.push(i);
+          num = num / i;
+        }
+
+        if(num/i == 1){
+        arr.push(num);
+        }
+      }
+      return arr;
+    }
+}
+
+let simpleImpl=getSimpleMn(base);
+let baseNumber=simpleImpl[simpleImpl.length-1];
+let res=0;
+let prom=0;
+
+for(let i=1;i<=number;i++){
+  prom=i;
+  while(prom%baseNumber==0){
+    prom=prom/baseNumber;
+    res++;
+  }
+}
+return res;
 }
