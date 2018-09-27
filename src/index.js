bfc73235a388775c7019a771b53ca7c514771738
Ray_Bruges <ogurezslava@gmail.com>
2018-09-28 00:27:11 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,33 +1,51 @@
-module.exports = function getZerosCount(number, base) {
+module.exports = function getZerosCount(number,base) {
+    
+    function getSimpleMn(num){
+        let arr = [];
+        if(num > 0 && num != 1){
+        
+            for(let i = 2; i <= num; i++){
+                while(num % i == 0 && num > 1){
+                    arr.push(i);
+                    num = num / i;
+                }
 
-  function getSimpleMn(num){
-    let arr = [];
-    if(num > 0 && num != 1){
-      for(let i = 2; i <= num; i++){
-        while(num % i == 0 && num > 1){
-          arr.push(i);
-          num = num / i;
+                if(num/i == 1){
+                arr.push(num);
+                }
+            }
+            return arr;
         }
-
-        if(num/i == 1){
-        arr.push(num);
-        }
-      }
-      return arr;
     }
-}
+    
+    let simpleImpl=getSimpleMn(base);
+    let simples=0;
+    let newSimpleImpl=[];
 
-let simpleImpl=getSimpleMn(base);
-let baseNumber=simpleImpl[simpleImpl.length-1];
-let res=0;
-let prom=0;
+    for(let i=0;i<simpleImpl.length;i++){
+        if(simpleImpl[i]==simpleImpl[i+1]){
+            simples +=simpleImpl[i];
+        }
+        else{
+            simples +=simpleImpl[i];
+            newSimpleImpl.push(simples);
+            simples=0;
+        }
+    }
 
-for(let i=1;i<=number;i++){
-  prom=i;
-  while(prom%baseNumber==0){
-    prom=prom/baseNumber;
-    res++;
-  }
-}
-return res;
+    newSimpleImpl.sort(function(a, b) {
+        return a - b;});
+
+    let baseNumber=newSimpleImpl[newSimpleImpl.length-1];
+    let res=0;
+    let prom=0;
+
+	for(let i=1;i<=number;i++){
+        prom=i;
+	    while(prom%baseNumber==0){
+            prom=prom/baseNumber;
+            res++;
+        }
+    }
+    return res;
 }
