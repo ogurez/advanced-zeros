41b4468f8f19f8f92cc97ecab336cbe84b9bdb81
Ray_Bruges <ogurezslava@gmail.com>
2018-10-01 23:52:19 +0300

    done

--- src/index.js
+++ src/index.js
@@ -16,18 +16,46 @@ module.exports = function getZerosCount(number, base) {
       return arr;
     }
 }
+let arr=getSimpleMn(base);
 
-let simpleImpl=getSimpleMn(base);
-let baseNumber=simpleImpl[simpleImpl.length-1];
-let res=0;
-let prom=0;
+let counter=0;
+let arrNew=[];
 
-for(let i=1;i<=number;i++){
-  prom=i;
-  while(prom%baseNumber==0){
-    prom=prom/baseNumber;
-    res++;
+for(let i=0; i<arr.length; i++){
+  if(arr[i]==arr[i+1]){
+    counter++;
+  }
+  else{
+    counter++;
+    arrNew.push(counter);
+    counter=0;
   }
 }
-return res;
+
+let prostMn=[];
+
+for(let i=0; i<arr.length; i++){
+  if(arr[i]==arr[i+1]){
+    continue;
+  }
+  else{
+    let counterMn=0;
+    let temp=number;
+
+    while((temp/arr[i])>0){
+      counterMn +=Math.floor(temp/arr[i]);
+      temp=Math.floor(temp/arr[i]);
+    }
+    prostMn.push(counterMn);
+  }
+}
+
+let counterZeros=0;
+for(let i=0;i<prostMn.length;i++){
+  prostMn[i]=Math.floor(prostMn[i]/arrNew[i]);
+}
+counterZeros=Math.min.apply(null,prostMn);
+
+return counterZeros;
+
 }
